4fd49e812d5098030f7b54632a34634e510f8f63
lelukas <lelukass@gmail.com>
2016-06-22 15:45:13 -0300

    commit

---
+++ MyIonic2Project/app/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+})
+export class HelloIonicPage {
+  names: string[];
+  
+  constructor() {
+    this.names = ['Pedro', 'João', 'Henrique', 'Mário'];
+  }
+}
