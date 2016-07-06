208ccda955a5caf537f884298c30aff722187fd6
Bruno Brito <bruno.brito.ln@gmail.com>
2016-07-06 16:07:09 -0300

    testando post request

--- app/pages/cadastro/cadastro.ts
+++ app/pages/cadastro/cadastro.ts
@@ -13,18 +13,29 @@ export class Cadastro {
     }
 
     constructor(private nav: NavController){
+        var endereco=
+        {"bairro" : $scope.bairro, "rua": $scope.rua, "estado" : $scope.estado, "cidade" : $scope.cidade, "numero" : $scope.numero};
+
+        var usuario =
+        {"nome" : $scope.nome, idade : $scope.idade, "sexo": $scope.sexo, "login" : $scope.login, "senha" : $scope.pass, endereco : endereco};
+
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http.post('https://expotaxi.herokuapp.com/usuario/add',
+            JSON.stringify(usuario),
+            {headers:headers})
+            .map((res: Response) => res.json())
+            .subscribe((res:Person) => this.postResponse = res);
 
     }
 
-    var endereco =
-        {"bairro" : $scope.bairro, "rua": $scope.rua, "estado" : $scope.estado, "cidade" : $scope.cidade, "numero" : $scope.numero};
 
-    var usuario =
-        {"nome" : $scope.nome, idade : $scope.idade, "sexo": $scope.sexo, "login" : $scope.login, "senha" : $scope.pass, endereco : endereco};
-
-    var xhr = new XMLHttpRequest();
+    /*var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://expotaxi.herokuapp.com/usuario/add", true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify(usuario));
+    xhr.send(JSON.stringify(usuario));*/
+
+
 
 }
